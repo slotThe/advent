c621f13788aab7a9bd431527598a3383fbb8e81a
slotThe <soliditsallgood@mailbox.org>
2021-12-20 14:04:14 +0100

    day20.clj: Comment

--- 2021/clojure-solutions/src/clojure_solutions/day20.clj
+++ 2021/clojure-solutions/src/clojure_solutions/day20.clj
@@ -18,7 +18,9 @@
   (for [x [-1 0 1], y [-1 0 1]]
     [(+ i y) (+ j x)]))
 
-(defn- grow [grid]
+(defn- grow
+  "Grow an `n × n' grid into an `n+1 × n+1' grid."
+  [grid]
   (let [[mn mx] ((fn [[x y]] [(dec x) (inc y)])
                  (reduce (fn [[xmin xmax] [x y]]
                            [(min xmin x) (max xmax x)])
@@ -26,31 +28,33 @@
     (mapcat (fn [x] [[x mn] [mx x] [x mx] [mn x]])
             (range mn (inc mx)))))
 
-(defn- step [{:keys [enhancement grid]}]
-  (map first
-       (iterate
-        (fn [[g n]]
-          (let [keys-g (keys g)
-                ;; If the first bit is one, the entire infinite grid
-                ;; will be "lit up" on every even iteration.
-                default (if (and (= 1 (enhancement 0)) (even? n))
-                          1
-                          0)]
-            (letfn [(lookup [x y]
-                      (->> (around x y)
-                           (map #(get g % default)) ; look up state of neighbours
-                           (coll-to-base 2) ; into decimal number for lookup
-                           enhancement))]   ; look up up new state
-              [(persistent!
-                (reduce (fn [acc [x y]] (assoc! acc [x y] (lookup x y)))
-                        (transient {})
-                        (concat keys-g (grow keys-g))))
-               (inc n)])))
-        [grid 1])))
+(defn- steps
+  "Return all steps in the game (of life) as an infinite list."
+  [{:keys [enhancement grid]}]
+  (let [ ;; If the first bit is one and the last one is zero, the entire
+        ;; infinite grid will be "lit up" on every even iteration.
+        special-even? (and (= 1 (enhancement 0))
+                           (= 0 (enhancement (dec (count enhancement)))))]
+    (map first                          ; get rid of iteration counter
+         (iterate
+          (fn [[g n]]
+            (let [keys-g (keys g)
+                  default (if (and (even? n) special-even?) 1 0)]
+              (letfn [(lookup [x y]
+                        (->> (around x y)
+                             (map #(get g % default)) ; look up state of neighbours
+                             (coll-to-base 2)         ; into decimal number for lookup
+                             enhancement))]           ; look up up new state
+                [(persistent!
+                  (reduce (fn [acc [x y]] (assoc! acc [x y] (lookup x y)))
+                          (transient {})
+                          (concat keys-g (grow keys-g))))
+                 (inc n)])))
+          [grid 1]))))
 
 (defn day20 []
-  (let [steps (step (parse))
-        score (fn [n] (->> (nth steps n)
+  (let [rounds (steps (parse))
+        score (fn [n] (->> (nth rounds n)
                            (filter-val (partial = 1))
                            count))]
     (println (score 2))
