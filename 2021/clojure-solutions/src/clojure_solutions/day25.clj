f6e35ff8607b80c0ae065d54c0473a74ddb982dd
slotThe <soliditsallgood@mailbox.org>
2021-12-25 21:44:54 +0100

    day25.clj: Cleanup

--- 2021/clojure-solutions/src/clojure_solutions/day25.clj
+++ 2021/clojure-solutions/src/clojure_solutions/day25.clj
@@ -12,26 +12,32 @@
                        nil)))
        (into {})))
 
-(defn- step [grid]
-  (let [[n m] (reduce (fn [[x-max y-max] [x y]]
-                        [(max x-max x) (max y-max y)])
-                      (keys grid))]
-    (letfn [(neighbour [g [[i j] dir]]
-              (let [candidate (case dir
-                                :right [(mod (inc i) (inc n)) j]
-                                :down [i (mod (inc j) (inc m))])]
-                (when-not (g candidate)
-                  {:orig [i j]
-                   :new candidate
-                   :dir dir})))
-            (update-place [g {:keys [orig new dir]}]
-              (assoc (dissoc g orig) new dir))]
-      (let [updates1 (keep #(neighbour grid %)
-                           (filter-val (partial = :right) grid))
-            new-grid (reduce update-place grid updates1)
-            updates2 (keep #(neighbour new-grid %)
-                           (filter-val (partial = :down) new-grid))]
-        (reduce update-place new-grid updates2)))))
+(defn- update-place [g {:keys [orig new dir]}]
+  (assoc (dissoc g orig) new dir))
+
+(defn- step [n m grid]
+  (let [neighbour (fn [g [[i j] dir]]
+                    (let [candidate (case dir
+                                      :right [(mod (inc i) n) j]
+                                      :down [i (mod (inc j) m)])]
+                      (when-not (g candidate)
+                        {:orig [i j]
+                         :new candidate
+                         :dir dir})))
+        ;; east-facing
+        updates1 (keep #(neighbour grid %)
+                       (filter-val (partial = :right) grid))
+        new-grid (reduce update-place grid updates1)
+        ;; south-facing
+        updates2 (keep #(neighbour new-grid %)
+                       (filter-val (partial = :down) new-grid))
+        final-grid (reduce update-place new-grid updates2)]
+    final-grid))
 
 (defn day25 []
-  (println (first (converge-when step (parse)))))
+  (let [grid (parse)
+        [n m] (map inc (reduce (fn [[x-max y-max] [x y]]
+                                 [(max x-max x) (max y-max y)])
+                               (keys grid)))]
+    ;; => 598
+    (println (first (converge-when (partial step n m) grid)))))
