02c00da04490de5a57b45e495389beda0c97e18b
Tony Zorman <soliditsallgood@mailbox.org>
2022-12-03 10:22:18 +0100

    Day 3: Clojure ❄

--- clojure-solutions/src/clojure_solutions/core.clj
+++ clojure-solutions/src/clojure_solutions/core.clj
@@ -1,7 +1,8 @@
 (ns clojure-solutions.core
   (:gen-class)
-  (:require [clojure-solutions.day1 :as day1])
-  (:require [clojure-solutions.day2 :as day2]))
+  (:require [clojure-solutions.day1 :as day1]
+            [clojure-solutions.day2 :as day2]
+            [clojure-solutions.day3 :as day3]))
 
 (defn- print-day [day one two]
   (println "!!! Day" day "!!!")
@@ -11,4 +12,5 @@
 
 (defn -main [& args]
   (print-day 1 (day1/get-nth-most-wanted 1) (day1/get-nth-most-wanted 3))
-  (print-day 2 (day2/day2 :one) (day2/day2 :two)))
+  (print-day 2 (day2/day2 :one) (day2/day2 :two))
+  (print-day 3 (day3/day3 :one) (day3/day3 :two)))
---
+++ clojure-solutions/src/clojure_solutions/day3.clj
@@ -0,0 +1,26 @@
+(ns clojure-solutions.day3
+  (:require [clojure.string :as str]
+            [clojure.set    :as set])
+  (:use [clojure-solutions.util] :reload))
+
+(defn- parse []
+  (str/split-lines (slurp "./input/day3.txt")))
+
+(defn- solve [fun]
+  (->> (parse)                           ; ["AA" "acD" …]
+       fun                               ; [[[\A \A] …] [[\c \Z \a] …] …]
+       (map #(map (partial into #{}) %)) ; [[#{\A} …] [#{\c \Z \a} …] …]
+       (map #(apply set/intersection %)) ; [#{\A} #{\Z} …]
+       (map (comp value first))          ; (+ 27 52 …)
+       sum))
+
+(defn- value [c]
+  (let [ascii (int c)]
+    (cond
+      (<= 97 ascii 122) (- ascii 96)
+      (<= 65 ascii 90 ) (- ascii 38))))
+
+(defn day3 [part]
+  (case part
+    :one (solve #(map (fn [s] (split-at (/ (count s) 2) s)) %))
+    :two (solve #(partition 3 %))))
