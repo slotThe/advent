5e2b286f884bab6236cb2a381759cd2f215d8c19
Tony Zorman <soliditsallgood@mailbox.org>
2023-04-09 13:20:19 +0200

    Rust: 2015/04
    I suppose the idea was to write one's own MD5 hash function, and I will
    do that—at some other point.

---
+++ aoc2015/rust-solutions/src/day4.rs
@@ -0,0 +1,17 @@
+use anyhow::Result;
+use std::str::pattern::Pattern;
+
+pub fn day4() -> Result<(usize, usize)> {
+    let inp = std::fs::read_to_string("../inputs/day4.txt")?;
+    Ok((solve("00000", inp.trim()), (solve("000000", inp.trim()))))
+}
+
+fn solve(pfx: &str, inp: &str) -> usize {
+    for i in 0.. {
+        let digest: String = format!("{:x}", md5::compute(inp.to_string() + &i.to_string()));
+        if pfx.is_prefix_of(&digest) {
+            return i;
+        }
+    }
+    panic!("2015/day4::solve: Unreachable")
+}
--- aoc2015/rust-solutions/src/main.rs
+++ aoc2015/rust-solutions/src/main.rs
@@ -1,13 +1,17 @@
+#![feature(pattern)]
+
 use anyhow::Result;
 use rust_aoc_util::print_day;
 
 pub mod day1;
 pub mod day2;
 pub mod day3;
+pub mod day4;
 
 fn main() -> Result<()> {
     print_day(1, day1::day1()?);
     print_day(2, day2::day2()?);
     print_day(3, day3::day3()?);
+    print_day(4, day4::day4()?);
     Ok(())
 }
