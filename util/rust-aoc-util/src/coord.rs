9edc359e2ddc7896ef112c0fdb8808681c5f7bd4
Tony Zorman <soliditsallgood@mailbox.org>
2023-04-11 12:25:42 +0200

    Rust/util: cargofmt

--- util/rust-aoc-util/src/coord.rs
+++ util/rust-aoc-util/src/coord.rs
@@ -1,38 +1,38 @@
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
 pub enum Dir {
-    North,
-    East,
-    South,
-    West,
+  North,
+  East,
+  South,
+  West,
 }
 
 pub fn char_to_dir(c: char) -> Option<Dir> {
-    match c {
-        '^' => Some(Dir::North),
-        '>' => Some(Dir::East),
-        'v' => Some(Dir::South),
-        '<' => Some(Dir::West),
-        _ => None,
-    }
+  match c {
+    '^' => Some(Dir::North),
+    '>' => Some(Dir::East),
+    'v' => Some(Dir::South),
+    '<' => Some(Dir::West),
+    _ => None,
+  }
 }
 
 // 2d coordinates: x grows to the *right* and y grows *down*.
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
 pub struct Coord {
-    x: i32,
-    y: i32,
+  x: i32,
+  y: i32,
 }
 
 impl Coord {
-    pub fn move_dir_mut(&mut self, d: Dir) {
-        match d {
-            Dir::North => self.y -= 1,
-            Dir::West => self.x -= 1,
-            Dir::East => self.x += 1,
-            Dir::South => self.y += 1,
-        }
+  pub fn move_dir_mut(&mut self, d: Dir) {
+    match d {
+      Dir::North => self.y -= 1,
+      Dir::West => self.x -= 1,
+      Dir::East => self.x += 1,
+      Dir::South => self.y += 1,
     }
+  }
 }
 
 pub const ORIGIN: Coord = Coord { x: 0, y: 0 };
--- util/rust-aoc-util/src/lib.rs
+++ util/rust-aoc-util/src/lib.rs
@@ -8,26 +8,26 @@ use nom::{combinator::all_consuming, Finish};
 
 pub fn parse<'a, R, F>(inp: &'a str, parser: F) -> Result<R, nom::error::Error<&'a str>>
 where
-    F: Fn(&'a str) -> nom::IResult<&'a str, R>,
+  F: Fn(&'a str) -> nom::IResult<&'a str, R>,
 {
-    all_consuming(parser)(inp).finish().map(|(_, r)| r)
+  all_consuming(parser)(inp).finish().map(|(_, r)| r)
 }
 
 ///////////////////////////////////////////////////////////////////////
 // Matrix util
 
 pub fn transpose<El: Clone>(xxs: Vec<Vec<El>>) -> Vec<Vec<El>> {
-    transpose_with(|v| v, xxs)
+  transpose_with(|v| v, xxs)
 }
 
 pub fn transpose_with<I: Clone, O, F>(f: F, xxs: Vec<Vec<I>>) -> Vec<Vec<O>>
 where
-    F: Fn(Vec<I>) -> Vec<O>,
+  F: Fn(Vec<I>) -> Vec<O>,
 {
-    (0..xxs[0].len())
-        .map(|i| xxs.iter().map(|row| row[i].clone()).collect())
-        .map(f)
-        .collect()
+  (0..xxs[0].len())
+    .map(|i| xxs.iter().map(|row| row[i].clone()).collect())
+    .map(f)
+    .collect()
 }
 
 ///////////////////////////////////////////////////////////////////////
@@ -35,34 +35,34 @@ where
 
 pub fn print_day<A, B>(num: usize, solutions: (A, B))
 where
-    A: AdventString,
-    B: AdventString,
+  A: AdventString,
+  B: AdventString,
 {
-    println!("!!! Day {num} !!!");
-    println!("First  task: {}", solutions.0.pp());
-    println!("Second task: {}\n", solutions.1.pp());
+  println!("!!! Day {num} !!!");
+  println!("First  task: {}", solutions.0.pp());
+  println!("Second task: {}\n", solutions.1.pp());
 }
 
 pub trait AdventString {
-    fn pp(&self) -> String;
+  fn pp(&self) -> String;
 }
 
 impl<A: ToString> AdventString for Option<A> {
-    fn pp(&self) -> String {
-        match self {
-            None => "None".to_string(),
-            Some(x) => x.to_string(),
-        }
+  fn pp(&self) -> String {
+    match self {
+      None => "None".to_string(),
+      Some(x) => x.to_string(),
     }
+  }
 }
 
 impl<A: ToString> AdventString for Result<A> {
-    fn pp(&self) -> String {
-        match self {
-            Err(e) => e.to_string(),
-            Ok(x) => x.to_string(),
-        }
+  fn pp(&self) -> String {
+    match self {
+      Err(e) => e.to_string(),
+      Ok(x) => x.to_string(),
     }
+  }
 }
 
 // Yup, this is really necessary, because apparently
@@ -72,31 +72,21 @@ impl<A: ToString> AdventString for Result<A> {
 // doesn't work and triggers E0119. Yikes.
 
 impl AdventString for String {
-    fn pp(&self) -> String {
-        self.to_string()
-    }
+  fn pp(&self) -> String { self.to_string() }
 }
 
 impl AdventString for i32 {
-    fn pp(&self) -> String {
-        self.to_string()
-    }
+  fn pp(&self) -> String { self.to_string() }
 }
 
 impl AdventString for u32 {
-    fn pp(&self) -> String {
-        self.to_string()
-    }
+  fn pp(&self) -> String { self.to_string() }
 }
 
 impl AdventString for u128 {
-    fn pp(&self) -> String {
-        self.to_string()
-    }
+  fn pp(&self) -> String { self.to_string() }
 }
 
 impl AdventString for usize {
-    fn pp(&self) -> String {
-        self.to_string()
-    }
+  fn pp(&self) -> String { self.to_string() }
 }
