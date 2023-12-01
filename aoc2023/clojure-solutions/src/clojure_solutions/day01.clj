474e809a100ee5415bb55e36588cb22cd5f0ccb2
Tony Zorman <soliditsallgood@mailbox.org>
2023-12-01 19:25:27 +0100

    2023/Clojure/01: Prettify

--- aoc2023/clojure-solutions/src/clojure_solutions/day01.clj
+++ aoc2023/clojure-solutions/src/clojure_solutions/day01.clj
@@ -2,33 +2,28 @@
   (:require [clojure.string :as str])
   (:use [clojure-aoc-util.util] :reload))
 
-(defn- get-last-two [twist xs]
-  (parse-long (str/join [(twist (first xs)) (twist (last xs))])))
-
 (defn- solve1 [input]
   (->> input
-       (map #(re-seq #"[1-9]" %))
-       (map (partial get-last-two identity))
+       (map (partial re-seq #"[1-9]"))
+       (map (fn [xs] (parse-long (str/join [(first xs) (last xs)]))))
        sum))
+
+(def replacements
+  ["one" "two" "three" "four" "five" "six" "seven" "eight" "nine" ])
 
 (defn- solve2 [input]
   (letfn [(to-digit [[d s]]
             (if (not (empty? d))
-              d
-              (case s
-                "one"   "1"
-                "two"   "2"
-                "three" "3"
-                "four"  "4"
-                "five"  "5"
-                "six"   "6"
-                "seven" "7"
-                "eight" "8"
-                "nine"  "9"
-                s)))]
+              (parse-long d)
+              (+ 1 (.indexOf replacements s))))]
     (->> input
-         (map #(re-seq #"[1-9]|(?=(one|two|three|four|five|six|seven|eight|nine))" %))
-         (map (partial get-last-two to-digit))
+         (map (partial re-seq
+                       (re-pattern (str/join ["[1-9]|(?=("
+                                              (str/join "|" replacements)
+                                              "))"]))))
+         (map (fn [xs] (+ (* (to-digit (first xs))
+                             10)
+                          (to-digit (last xs)))))
          sum)))
 
 (defn day01 [part]
