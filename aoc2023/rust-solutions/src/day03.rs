8f745592f82406b7ea5dc8295290034a41bdb952
Tony Zorman <soliditsallgood@mailbox.org>
2023-12-03 20:49:18 +0100

    2023/Rust/Day03: Cleanup

--- aoc2023/rust-solutions/src/day03.rs
+++ aoc2023/rust-solutions/src/day03.rs
@@ -3,15 +3,15 @@ use rust_aoc_util::iter_to_num;
 
 pub fn day03() -> (usize, usize) {
   let inp = std::fs::read_to_string("../inputs/day03.txt").unwrap();
-  let inp: Vec<(char, Vec<usize>)> =
+  let char_map: Vec<(char, Vec<usize>)> =
     solve(inp.lines().map(|l| l.chars().collect()).collect());
 
   (
-    inp
+    char_map
       .iter()
       .map(|(_, v)| v.iter().sum::<usize>())
       .sum::<usize>(),
-    inp
+    char_map
       .iter()
       .filter_map(|(s, v)| {
         if *s == '*' && v.len() == 2 {
@@ -27,26 +27,32 @@ pub fn day03() -> (usize, usize) {
 fn solve(inp: Vec<Vec<char>>) -> Vec<(char, Vec<usize>)> {
   let width = inp.len();
   let height = inp[0].len();
+  let is_digit = |i: usize, j: usize| -> bool {
+    inp
+      .get(i)
+      .and_then(|row| row.get(j))
+      .is_some_and(|c| c.is_ascii_digit())
+  };
   let mut res: Vec<(char, Vec<usize>)> = vec![];
+
   (0..width)
     .cartesian_product(0..height)
-    .filter(|(i, j)| !(inp[*i][*j] == '.' || inp[*i][*j].is_ascii_digit()))
+    .filter(|&(i, j)| !(inp[i][j] == '.' || is_digit(i, j))) // is_symbol
     .for_each(|(i, j)| {
       let mut sym_nums: Vec<Vec<(usize, usize)>> = vec![];
       neighbours8(i, j)
-        .into_iter()
-        .filter(|(k, l)| inp[*k][*l].is_ascii_digit())
+        .filter(|&(k, l)| is_digit(k, l))
         .for_each(|(k, l)| {
-          // Find start of number.
+          // Find start (leftmost position) of number.
           let start_of_num = (0..)
-            .take_while(|off| is_digit(&inp, k, l - off))
+            .take_while(|offset| is_digit(k, l - offset))
             .last()
             .unwrap();
           // Save *coordinates of* the number.
           sym_nums.push(
             (0..)
-              .map(|off| (k, l - start_of_num + off))
-              .take_while(|(rk, rl)| is_digit(&inp, *rk, *rl))
+              .map(|offset| (k, l - start_of_num + offset))
+              .take_while(|&(x, y)| is_digit(x, y))
               .collect(),
           );
         });
@@ -58,14 +64,15 @@ fn solve(inp: Vec<Vec<char>>) -> Vec<(char, Vec<usize>)> {
         inp[i][j], // symbol
         sym_nums // value of number
           .iter()
-          .map(|v| iter_to_num(v.iter().map(|(x, y)| inp[*x][*y])))
+          .map(|v| iter_to_num(v.iter().map(|&(x, y)| inp[x][y])))
           .collect(),
       ));
     });
+
   res
 }
 
-fn neighbours8(i: usize, j: usize) -> Vec<(usize, usize)> {
+fn neighbours8(i: usize, j: usize) -> impl Iterator<Item = (usize, usize)> {
   vec![
     (i, j + 1),
     (i, j - 1),
@@ -76,10 +83,5 @@ fn neighbours8(i: usize, j: usize) -> Vec<(usize, usize)> {
     (i - 1, j),
     (i - 1, j + 1),
   ]
-}
-
-fn is_digit(v: &[Vec<char>], i: usize, j: usize) -> bool {
-  v.get(i)
-    .and_then(|v2| v2.get(j))
-    .is_some_and(|d| d.is_ascii_digit())
+  .into_iter()
 }
