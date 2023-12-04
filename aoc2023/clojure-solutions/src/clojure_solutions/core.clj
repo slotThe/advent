dbb0c43e1c4466f8caccdeab1cad4c290b2e6bff
Tony Zorman <soliditsallgood@mailbox.org>
2023-12-04 13:10:33 +0100

    2023/Clojure: Day 04

--- aoc2023/clojure-solutions/src/clojure_solutions/core.clj
+++ aoc2023/clojure-solutions/src/clojure_solutions/core.clj
@@ -1,7 +1,11 @@
 (ns clojure-solutions.core
   (:gen-class)
-  (:require [clojure-solutions.day01 :as day01])
+  (:require [clojure-solutions.day01 :as day01]
+            [clojure-solutions.day03 :as day03]
+            [clojure-solutions.day04 :as day04])
   (:use [clojure-aoc-util.util] :reload))
 
 (defn -main [& _]
-  (print-day 1 (day01/day01 :one) (day01/day01 :two)))
+  (print-day 1 (day01/day01 :one) (day01/day01 :two))
+  (print-day 3 (day03/day03 :one) (day03/day03 :two))
+  (print-day 4 (day04/day04 :one) (day04/day04 :two)))
---
+++ aoc2023/clojure-solutions/src/clojure_solutions/day04.clj
@@ -0,0 +1,39 @@
+(ns clojure-solutions.day04
+  (:require [clojure.string :as str]
+            [clojure-aoc-util.coords :as coord]
+            [clojure.set :as set])
+  (:use [clojure-aoc-util.util] :reload))
+
+(defn- parse []
+  (into {}
+        (comp (map #(str/split % #"\|"))
+              (map (partial map #(map read-string (re-seq #"\d+" %))))
+              (map (fn [[have winning]]
+                     {(nth have 0)              ; card number
+                      [1                        ; multiplicity
+                       (count (set/intersection ; number of winning guesses
+                               (into #{} (drop 1 have))
+                               (into #{} winning)))]})))
+        (str/split-lines (slurp "../inputs/day04.txt"))))
+
+(defn- calc-points [[_ [_ correct-guesses]]]
+  (int (Math/pow 2 (dec correct-guesses))))
+
+(defn- part2 [hm]
+  (let [max (count hm)]
+    (map (fn [[_ [mult _]]] mult)
+         (loop [cards hm, n 1]
+           (if (> n max)
+             cards
+             (let [[mult correct-guesses] (get cards n)
+                   wins (take correct-guesses (iterate inc (inc n)))]
+               (recur (reduce #(update %1 %2 (fn [[n cg]] [(+ mult n) cg]))
+                              cards
+                              wins)
+                      (inc n))))))))
+
+(defn day04 [p]
+  (let [cards (parse)]
+    (sum (case p
+           :one (map calc-points cards)
+           :two (part2 cards)))))
