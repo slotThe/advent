401dc304784668aa16c217d7bd9a15ddf6a9eadf
Tony Zorman <soliditsallgood@mailbox.org>
2023-12-06 08:26:20 +0100

    2023/Clojure: Day 06

--- aoc2023/clojure-solutions/src/clojure_solutions/core.clj
+++ aoc2023/clojure-solutions/src/clojure_solutions/core.clj
@@ -3,11 +3,13 @@
   (:require [clojure-solutions.day01 :as day01]
             [clojure-solutions.day03 :as day03]
             [clojure-solutions.day04 :as day04]
-            [clojure-solutions.day05 :as day05])
+            [clojure-solutions.day05 :as day05]
+            [clojure-solutions.day06 :as day06])
   (:use [clojure-aoc-util.util] :reload))
 
 (defn -main [& _]
   (print-day 1 (day01/day01 :one) (day01/day01 :two))
   (print-day 3 (day03/day03 :one) (day03/day03 :two))
   (print-day 4 (day04/day04 :one) (day04/day04 :two))
-  (print-day 5 (day05/day05 :one) (day05/day05 :two)))
+  (print-day 5 (day05/day05 :one) (day05/day05 :two))
+  (print-day 6 (day06/day06 :one) (day06/day06 :two)))
---
+++ aoc2023/clojure-solutions/src/clojure_solutions/day06.clj
@@ -0,0 +1,32 @@
+(ns clojure-solutions.day06
+  (:require [clojure.string :as str]
+            [clojure-aoc-util.coords :as coord]
+            [clojure.set :as set])
+  (:use [clojure-aoc-util.util] :reload))
+
+(defn- parse1 [inp]
+  (let [[as bs] (map #(re-seq #"\d+" %) (str/split-lines inp))]
+    (map (fn [a b] (map read-string [a b])) as bs)))
+
+(defn- parse2 [inp]
+  (->> (str/split-lines inp)
+       (map (comp str/join (partial re-seq #"\d+")))
+       (map read-string)))
+
+(defn- solve [inp]
+  "Note that ½t is the best time we can do. Hence, we merely need to look for
+  the index below and above the middle where the curve dips below the (best)
+  distance."
+  (let [[time distance] inp
+        mid (Math/divideExact time 2)]
+    (letfn [(beats-best? [t]
+              (> (* t (- time t))
+                 distance))]
+      (- (first (binary-search (comp not beats-best?) mid time)) ; first time we dip below
+         (first (binary-search beats-best?            0   mid)))))) ; first time we dip above
+
+(defn day06 [p]
+  (let [inp (slurp "../inputs/day06.txt")]
+    (case p
+      :one (reduce * (map solve (parse1 inp)))
+      :two (solve (parse2 inp)))))
