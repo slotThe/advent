cabcdccc3ab106c4797866a598917ddc194c0b26
Tony Zorman <soliditsallgood@mailbox.org>
2023-12-22 10:31:46 +0100

    2023/Haskell: Parallelise main function

--- aoc2023/haskell-solutions/app/Main.hs
+++ aoc2023/haskell-solutions/app/Main.hs
@@ -1,4 +1,4 @@
-import Util
+import Control.Parallel.Strategies (parMap, rseq)
 import Day01
 import Day02
 import Day07
@@ -6,13 +6,15 @@ import Day09
 import Day16
 import Day19
 import Day22
+import Util
 
 main :: IO ()
-main = do
-  day 1  day01
-  day 2  day02
-  day 7  day07
-  day 9  day09
-  day 16 day16
-  day 19 day19
-  day 22 day22
+main = sequenceA_ . parMap rseq (uncurry day) $
+  [ (1,  day01)
+  , (2,  day02)
+  , (7,  day07)
+  , (9,  day09)
+  , (16, day16)
+  , (19, day19)
+  , (22, day22)
+  ]
