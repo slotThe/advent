8498858b9e3472cdd3fec6aff86f4a0ce5816fab
Tony Zorman <mail@tony-zorman.com>
2024-11-09 11:28:03 +0100

    2021/flakes: Bump

--- aoc2021/clojure-solutions/flake.nix
+++ aoc2021/clojure-solutions/flake.nix
@@ -1,27 +1,19 @@
 {
-  inputs = {
-    nixpkgs.url     = "github:NixOS/nixpkgs/nixos-23.05";
-    flake-utils.url = "github:numtide/flake-utils";
-    clj-nix = {
-      url = "github:jlesquembre/clj-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-  outputs = { self, nixpkgs, flake-utils, clj-nix }:
-    flake-utils.lib.eachDefaultSystem (system: {
-      packages = {
-        default = clj-nix.lib.mkCljApp {
-          pkgs    = nixpkgs.legacyPackages.${system};
-          modules = [
-            {
-              projectSrc         = ./.;
-              name               = "com.github.slotThe/advent";
-              main-ns            = "clojure-solutions.core";
-              nativeImage.enable = true;
-            }
-          ];
-        };
+  outputs = { self, nixpkgs }:
+    let
+      system  = "x86_64-linux";
+      pkgs    = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+              jdk
+              clojure
+              leiningen
+              clj-kondo
+              cljfmt
+        ];
       };
-    });
+    };
 }
