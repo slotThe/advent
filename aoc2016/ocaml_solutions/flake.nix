889e5762e793871d76ce71d9250f3bd291a77952
Tony Zorman <mail@tony-zorman.com>
2025-10-13 07:50:46 +0200

    2016/OCaml: Use opam-based flake

--- aoc2016/ocaml_solutions/flake.nix
+++ aoc2016/ocaml_solutions/flake.nix
@@ -1,38 +1,23 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-
   outputs = { self, nixpkgs }:
     let
       system  = "x86_64-linux";
       pkgs    = nixpkgs.legacyPackages.${system};
     in {
-      # nix develop
       devShells.${system}.default = pkgs.mkShell {
         buildInputs = with pkgs; [
-          (dune_3.overrideAttrs(old: {
-              version = "3.17.0";
-              src = fetchFromGitHub {
-                owner = "ocaml";
-                repo = "dune";
-                rev = "b61e9d612642dbd1fd58d25c90126d607e69bd43";
-                hash = "sha256-XLJ3anchLhogOD2AiAi12950xRWeitERTZ21YpaY5SU=";
-              };
-            }))
+          dune_3
+          opam
           ocaml
           ocamlPackages.findlib
           ocamlPackages.utop
-          ocamlPackages.merlin
-          ocamlPackages.odoc
-          ocamlPackages.ounit
-          ocamlPackages.ocaml-lsp
-          ocamlPackages.base
-          ocamlPackages.core
-          ocamlPackages.stdio
-          ocamlPackages.angstrom
-          ocamlPackages.re
-          ocamlPackages.ppx_jane
-          ocamlformat
         ];
+        shellHook = ''
+          export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib.outPath}/lib:$LD_LIBRARY_PATH";
+          test -d _opam || opam switch create .
+          eval $(opam env)
+        '';
       };
     };
 }
