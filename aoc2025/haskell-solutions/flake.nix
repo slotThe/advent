b159f9002c47f467ada8d208d92e230cd93e8131
Tony Zorman <mail@tony-zorman.com>
2025-11-17 13:44:10 +0100

    2025/Haskell: Init

---
+++ aoc2025/haskell-solutions/flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      package = "haskell-solutions";
+      system  = "x86_64-linux";
+      pkgs    = nixpkgs.legacyPackages.${system};
+      overlay = final: prev: {
+        main = prev.callCabal2nix package ./. { };
+      };
+      haskellPackages = pkgs.haskellPackages.extend overlay;
+    in {
+      # nix build
+      packages.${system}.default = haskellPackages.main;
+
+      # nix develop
+      devShells.${system}.default = haskellPackages.shellFor {
+        packages = p: [ p.main ];
+        buildInputs = with haskellPackages; [
+          cabal-install
+          haskell-language-server
+        ];
+      };
+    };
+}
