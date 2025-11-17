40a176515f642cb5c05b3a582dd852608ca1e1a1
Tony Zorman <mail@tony-zorman.com>
2025-11-17 13:44:41 +0100

    2025/OCaml: Init

---
+++ aoc2025/ocaml_solutions/flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  outputs = { self, nixpkgs }:
+    let
+      system  = "x86_64-linux";
+      pkgs    = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          dune_3
+          opam
+          ocaml
+          ocamlPackages.findlib
+          ocamlPackages.utop
+        ];
+             shellHook = ''
+          export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib.outPath}/lib:$LD_LIBRARY_PATH";
+          test -d _opam || opam switch create .
+          eval $(opam env)
+        '';
+      };
+    };
+}
